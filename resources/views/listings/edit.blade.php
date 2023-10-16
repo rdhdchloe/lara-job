786e47fc7ce95a66742fb4fb38bc73876fd9d702
chloetakahashi <chloe.socrates.7z@gmail.com>
2023-10-17 02:27:31 +0900

    ボタンの表示を調整する

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -88,7 +88,7 @@
           
               <div class="mt-6 flex items-center justify-end gap-x-6">
                 <a href="{{ route('listings.show', ['listing'=> $listing->id] )}}" class="text-sm font-semibold leading-6 text-gray-900">キャンセル</a>
-                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">更新する</button>
+                <button type="submit" class="action-btn">更新する</button>
               </div>
           
               </form>
