5d5f0a1a96030469ea9e907dd347788c6370fdca
chloetakahashi <chloe.socrates.7z@gmail.com>
2023-10-21 23:25:54 +0900

    Yahoo Job APIをseedファイルに導入する

--- database/seeders/ListingTableSeeder.php
+++ database/seeders/ListingTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ListingTableSeeder extends Seeder
 {
@@ -12,7 +14,7 @@ class ListingTableSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Listing::factory(30)->create();
+        // \App\Models\Listing::factory(30)->create();
 
         // Listing::create([
         //     'title'=>'Laravel developer',
@@ -22,5 +24,33 @@ class ListingTableSeeder extends Seeder
         //     'url'=>'https://www.google.com',
         //     'location'=> 'Tokyo'
         // ]);
+
+        $appId = env('YAHOO_API_KEY');
+        $results = 1000;
+
+        $response = Http::get('https://job.yahooapis.jp/v1/furusato/jobinfo/', [
+            'appid' => $appId,
+            'results' => $results,
+        ]);
+        // dd($response);
+
+        if($response->successful()){
+            $response = $response->json();
+            $jobData = $response['results'];
+            foreach ($jobData as $job) {
+                if (isset($job['occupationCode']) && $job['occupationCode'] === '104') {
+                    Listing::create([
+                        'title' => $job['title'],
+                        'description' => $job['description'],
+                        'company' => $job['cpName'],
+                        'position' => $job['occupationName'] ? $job['occupationName'] : 'webエンジニア',
+                        'url' => $job['receptionUrl'] ? $job['receptionUrl'] : 'メールもしくは電話応募',
+                        'salary' => ($job['salaryMin'] && $job['salaryMax']) ? $job['salaryMin'] . ' 〜 ' . $job['salaryMax'] : '保有されるスキル・経験・能力により決定 (その他)',
+                        'location' => $job['workLocationPrefecture'],
+                        'user_id' => 1
+                    ]);
+                };
+            }
+        }
     }
 }
