7bbf06e546fe8bf0440ecad20a8aa83be4c43fcf
chloetakahashi <chloe.socrates.7z@gmail.com>
2023-11-03 22:16:32 +0900

    人気順でソートする機能を追加する

--- resources/views/livewire/listing-list.blade.php
+++ resources/views/livewire/listing-list.blade.php
@@ -5,6 +5,8 @@
           wire:click="sortListing('desc')">新しい順</button>
           <button class="{{ $sort === 'asc' ? 'sort-current' : 'sort-default'}} sort-nav"
           wire:click="sortListing('asc')">古い順</button>
+          <button class="{{ $popular ? 'sort-current' : 'sort-default'}} sort-nav"
+          wire:click="togglePopular">人気順</button>
         </nav>
     </div>
     <div class="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 my-10">
